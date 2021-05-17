11258f4c000460d390bd60be092217599faec1a8
bwcs99 <blazejw0@o2.pl>
2021-05-17 22:17:04 +0200

    Full beta version

--- app/src/main/java/com/example/projektzespolowy/MainActivity.java
+++ app/src/main/java/com/example/projektzespolowy/MainActivity.java
@@ -1,33 +1,45 @@
 package com.example.projektzespolowy;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
-import android.app.Dialog;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.Button;
 import android.widget.GridView;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import java.text.BreakIterator;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.File;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
-    private String PATH_TAG = "PATHS_TO_FILES";
-
-    private ArrayList<String> paths = new ArrayList<>();
+    private ArrayList<PhotoItem> items = new ArrayList<>();
+    private boolean isOrderReversed = false;
     Map tags = new HashMap<String, Integer>();
+
+    public GridView gridView = null;
+
+
+    private final String ARR_TAG = "ITEMS_ARRAY";
+    private final String ORDER_TAG = "CURRENT_ORDER";
+
 
 
     @Override
@@ -35,12 +47,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        GridView gridView = findViewById(R.id.myGrid);
-        gridView.setAdapter(new ImageAdaptor(paths, this));
+        gridView = findViewById(R.id.myGrid);
+
+        isOrderReversed = loadOrder();
+
+        loadImageData();
+
+        if(items.size() != 0){
+            sortItems();
+        }
+
+        gridView.setAdapter(new ImageAdapter(items, this));
 
         gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick ( AdapterView < ? > parent, View view, int position, long id ) {
+
+                Intent intent = new Intent(getApplicationContext(), ImagesDetails.class);
+
+                intent.putParcelableArrayListExtra(getString(R.string.PARCELABLE_TAG), items);
+                startActivity(intent);
+
 
             }
         });
@@ -48,12 +75,130 @@ public class MainActivity extends AppCompatActivity {
         gridView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
             public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+
+                showAlertDialog(position);
                 return false;
+            }
+        });
+
+        findViewById(R.id.button2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                sortItems();
             }
         });
     }
 
-    public void reverseSort(View view){
+    public void saveImageData(ArrayList<PhotoItem> list){
+
+        Gson gson = new Gson();
+        SharedPreferences sp = getSharedPreferences(getString(R.string.imagesData), MODE_PRIVATE);
+        String listJSON = gson.toJson(list);
+
+        SharedPreferences.Editor editor = sp.edit();
+
+        editor.putString(getString(R.string.savedImages), listJSON);
+        editor.apply();
+
+    }
+
+    public void loadImageData(){
+
+        Gson gson = new Gson();
+        SharedPreferences sp = getSharedPreferences(getString(R.string.imagesData), MODE_PRIVATE);
+        Type type = new TypeToken<ArrayList<PhotoItem>>() {}.getType();
+
+        String myJSON = sp.getString(getString(R.string.savedImages), null);
+
+        if(myJSON != null){
+            items = gson.fromJson(myJSON, type);
+        }  else {
+            items = new ArrayList<PhotoItem>();
+        }
+
+    }
+
+    public void saveOrder(boolean newOrder){
+
+        SharedPreferences sp = getSharedPreferences(getString(R.string.orderState), MODE_PRIVATE);
+        SharedPreferences.Editor editor = sp.edit();
+
+        editor.putBoolean(getString(R.string.orderValue), newOrder);
+        editor.apply();
+
+    }
+
+    public boolean loadOrder(){
+
+        SharedPreferences sp = getSharedPreferences(getString(R.string.orderState), MODE_PRIVATE);
+
+        return sp.getBoolean(getString(R.string.orderValue), false);
+    }
+
+    private void showAlertDialog(int position){
+
+        final boolean[] isDeleted = {false};
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        builder.setMessage(R.string.message1)
+                .setTitle(R.string.title1);
+
+        builder.setPositiveButton(R.string.yes1, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                File fileToDelete = new File(items.get(position).getPath().trim());
+                isDeleted[0] = fileToDelete.delete();
+                items.remove(position);
+
+                if(isDeleted[0]){
+                    saveImageData(items);
+                    changeAdapter();
+                }
+                Log.d("PZ", "Image successfully removed !");
+
+            }
+        });
+
+        builder.setNegativeButton(R.string.no1, new DialogInterface.OnClickListener(){
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                return;
+
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+
+    }
+
+    private void changeAdapter(){
+        gridView.setAdapter(new ImageAdapter(items, this));
+    }
+
+    public void sortItems(){
+
+        if(isOrderReversed){
+
+            Collections.sort(items);
+            isOrderReversed = false;
+            saveOrder(isOrderReversed);
+            changeAdapter();
+
+        } else {
+
+            Collections.sort(items, Collections.reverseOrder());
+            isOrderReversed = true;
+            saveOrder(isOrderReversed);
+            changeAdapter();
+        }
+
+        saveOrder(isOrderReversed);
+
     }
 
 
@@ -67,26 +212,31 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 
-        String path = null;
-
         super.onActivityResult(requestCode, resultCode, data);
+
+        String path = null;
 
         if(resultCode == Activity.RESULT_OK){
 
             if(requestCode == 1){
 
-                ArrayList<String> temp =  null;
+                ArrayList<PhotoItem> temp =  null;
 
-                 temp = data.getStringArrayListExtra(PATH_TAG);
+                String PATH_TAG = "PATHS_TO_FILES";
+                temp = data.getParcelableArrayListExtra(PATH_TAG);
 
                if(temp != null){
 
-                   for(String p : temp){
-                       paths.add(p);
-                   }
+                   items.addAll(temp);
 
-                   Toast.makeText(this, "Received not null path", Toast.LENGTH_LONG).show();
+               }
 
+               saveImageData(items);
+
+               if(gridView != null){
+
+                   sortItems();
+                   changeAdapter();
                }
 
 
@@ -94,4 +244,22 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+
+        outState.putParcelableArrayList(ARR_TAG, items);
+        outState.putBoolean(ORDER_TAG, isOrderReversed);
+
+        super.onSaveInstanceState(outState);
+    }
+
+    public void onRestoreInstanceState(Bundle savedInstanceState){
+        super.onRestoreInstanceState(savedInstanceState);
+
+        items = savedInstanceState.getParcelableArrayList(ARR_TAG);
+        isOrderReversed = savedInstanceState.getBoolean(ORDER_TAG);
+
+        sortItems();
+
+    }
 }
